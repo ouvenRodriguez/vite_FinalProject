ddf4e3354f4acf3e18b6d9f270bf488cff5a52e6
Ouven Rodríguez <ouven.rodriguez.c@gmail.com>
2025-05-12 11:21:57 -0500

    feat actulizarcion del from

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-/* import Login from './Pages/Login/Login'; */
-/* import Register from './Pages/Register/Register'; */
-import Home from './Pages/Home/Home';
-import ListaDocentes from './Components/ListaDocentes/ListaDocentes';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Home from './Pages/Home/Home'
+/* import ListaDocentes from './Components/ListaDocentes/ListaDocentes'; */
 import './App.css';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/docentes" element={<ListaDocentes />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
 
-      
-    
+     
     </Routes>
   );
 };
--- src/Components/Form/LoginForm.jsx
+++ src/Components/Form/LoginForm.jsx
@@ -4,24 +4,26 @@ import "./style.css";
 
 const LoginForm = () => {
   return (
-      <div id="login-form">
-        <h1>Iniciar Sesión</h1>
-        <p>Bienvenido de nuevo</p>
-        <form >
-         <div className="form-group">
-            <input type="text" required />
-            <label>Correo Electrónico</label>
-          </div>
-          <div className="form-group">
-            <input type="password" required />
-            <label>Contraseña</label>
-          </div>
-          <button type="submit">Iniciar Sesión</button>
-          <div id="login-footer">
-        <p>¿No tienes cuenta? <Link to="/register">Registrate</Link></p>
-      </div>
-        </form>
-      </div>
+    <div id="login-form">
+      <h1>Iniciar Sesión</h1>
+      <p>Bienvenido de nuevo</p>
+      <form>
+        <div className="form-group">
+          <input type="text" required />
+          <label>Correo Electrónico</label>
+        </div>
+        <div className="form-group">
+          <input type="password" required />
+          <label>Contraseña</label>
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+        <div id="login-footer">
+          <p>
+            ¿No tienes cuenta? <Link to="/register">Registrate</Link>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 
